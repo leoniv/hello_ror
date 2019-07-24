63d14400935fb04bb8ec849ef545d07f939818fc
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-07-25 00:47:43 +0700

    Genre in progress

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,10 +2,35 @@ class Genre < ApplicationRecord
   validates :name, presence: true
 
   def name=(val)
-    super self.class.normalize(val.to_s)
+    super self.class.normalize(val)
+  end
+
+  def self.map(*names)
+    names.map do |name|
+      nname = normalize name
+      if exists? nname
+        find nname
+      elsif !nname.empty? && block_given?
+        yield name, nname
+      end
+    end.compact
+  end
+
+  def self.map!(*names)
+    map(*names) do |_, nname|
+      create(name: nname)
+    end
+  end
+
+  def self.find(*names)
+    super *names.map {|val| normalize val}
+  end
+
+  def self.exists?(name)
+    super normalize(name)
   end
 
   def self.normalize(str)
-    str.gsub(/[\n\r]/, ' ').gsub(/\s+/, ' ').strip.downcase
+    str.to_s.gsub(/[\n\r]/, ' ').gsub(/\s+/, ' ').strip.downcase
   end
 end

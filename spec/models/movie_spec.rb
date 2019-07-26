2fe3fb5318f729f90a10ec73a8c09c1ece2c7835
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-07-26 05:55:20 +0000

    add countries seeds

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Movie, type: :model do
     .in_range(1895 .. Date.today.year + 5)}
   it { should validate_inclusion_of(:rating).in_range(0 .. 10) }
   it { should have_and_belong_to_many :genres }
-  it { should have_and_belong_to_many(:countries_of_production)
-    .class_name(:Country) }
+  it { should have_many(:countries_of_production)
+        .through(:countries_of_production)
+        .class_name(:Country) }
   it { should have_one_attached :cover_image }
 end

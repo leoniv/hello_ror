6409a1a372652b1f2e60103adba824ef64f7048b
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-07-30 22:49:31 +0700

    add root route

--- spec/routing/movies_routing_spec.rb
+++ spec/routing/movies_routing_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe MoviesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
       expect(:get => "/movies").to route_to("movies#index")
+      expect(:get => "/").to route_to("movies#index")
     end
 
     it "routes to #show" do
       expect(:get => "/movies/1").to route_to("movies#show", :id => "1")
     end
-
 
     it "routes to #create" do
       expect(:post => "/movies").to route_to("movies#create")
